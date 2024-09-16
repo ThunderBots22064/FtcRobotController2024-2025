45e67dc627911ca54eff447eda93aca15fc0107b
blueofficer <129885284+blueofficer@users.noreply.github.com>
2024-09-16 12:11:56 -0500

    adding docmatation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -7,13 +7,29 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name = "_2324BLOCKCODE (Blocks to Java)")
 public class Test extends LinearOpMode {
 
-    double BL2;
-    double FL2;
-    double Arm_speed;
-    double MAX_SPEED;
-    double FR2;
-    double BR2;
-    double error_corection;
+    //motar speeds
+    double BL_Power;
+    double FL_Power;
+    double FR_Power;
+    double BR_Power;
+    double ARM_Power;
+    // THIS IS TO SET MAX SPEED
+    final double BL_Max_Power = 0.80;
+    final double BR_Max_Power = 0.80;
+    final double FL_Max_Power = 0.80;
+    final double FR_Max_Power = 0.80;
+    // this is for error correction
+    double Fl_Error_Correction;
+    double FR_Error_Correction;
+    double Bl_Error_Correction;
+    double BR_Error_Correction;
+
+    public void Partal_reset_mem(){
+        BL_Power = 0;
+        FL_Power = 0;
+        FR_Power = 0;
+        BR_Power = 0;
+    }
 
     /**
      * This function is executed when this OpMode is selected from the Driver Station.
@@ -21,93 +37,77 @@ public class Test extends LinearOpMode {
     @Override
     public void runOpMode() {
 
+        // Setting up motars
         DcMotor BL = hardwareMap.get(DcMotor.class, "BL");
         DcMotor hook = hardwareMap.get(DcMotor.class, "Hook");
         DcMotor BR = hardwareMap.get(DcMotor.class, "BR");
         DcMotor FL = hardwareMap.get(DcMotor.class, "FL");
         DcMotor FR = hardwareMap.get(DcMotor.class, "FR");
 
+        // reset the motar config (might do something idk)
         FL.resetDeviceConfigurationForOpMode();
         FR.resetDeviceConfigurationForOpMode();
         BL.resetDeviceConfigurationForOpMode();
         BR.resetDeviceConfigurationForOpMode();
 
-//        FL.setDirection(DcMotor.Direction.REVERSE);
-        BR.setDirection(DcMotor.Direction.REVERSE);
-//        BL.setDirection(DcMotor.Direction.REVERSE);
+        // sets this motar direction to reverse the movement so 1 and -1 reveser speed
+        BR.setDirection(DcMotor.Direction.REVERSE); // doesnt work unless its like this
+        //Witch one is reversed depends on rotation that the motar is placed in (rotate 180 around the motartr's power cable)
 
 
-        // THIS IS TO SET MAX SPEED
-        MAX_SPEED = 0.35;
-        Arm_speed = 0.25;
+
 
         waitForStart();
         if (opModeIsActive()) {
             // Put run blocks here.
             while (opModeIsActive()) {
+                //debug information
                 telemetry.addData("Raw_left_stick_y", Float.toString(gamepad1.left_stick_y));
                 telemetry.addData("Raw_left_stick_y", Float.toString(gamepad1.left_stick_x));
                 telemetry.addData("\nbreak", "\n");
                 telemetry.addData("Raw_right_stick_y", Float.toString(gamepad1.right_stick_y));
                 telemetry.addData("Raw_right_stick_x", Float.toString(gamepad1.right_stick_x));
-                telemetry.update();
-                // FROINT AND BACK
-                BL2 += gamepad1.left_stick_y;
-                BR2 += gamepad1.left_stick_y;
-                FL2 += gamepad1.left_stick_y;
-                FR2 += gamepad1.left_stick_y;
-                // TURN LEFT AND RIGHT
-                BL2 += gamepad1.left_trigger + gamepad1.right_trigger * -1;
-                BR2 += gamepad1.right_trigger + gamepad1.left_trigger * -1;
-                FL2 += gamepad1.left_trigger + gamepad1.right_trigger * -1;
-                FR2 += gamepad1.right_trigger + gamepad1.left_trigger * -1;
-                //  MOVE LEFT AND RIGHT
-                BL2 += gamepad1.right_stick_x;
-                BR2 += gamepad1.right_stick_x * -1;
-                FL2 += gamepad1.right_stick_x * -1;
-                FR2 += gamepad1.right_stick_x;
-                // CHANGE MAX SPEED
-                if (gamepad1.dpad_down) {
-                    MAX_SPEED = MAX_SPEED * 100;
-                    MAX_SPEED = MAX_SPEED - 10;
-                    MAX_SPEED = MAX_SPEED / 100;
-                    sleep(100);
-                }
-                if (gamepad1.dpad_up) {
-                    MAX_SPEED = MAX_SPEED * 100;
-                    MAX_SPEED = MAX_SPEED + 10;
-                    MAX_SPEED = MAX_SPEED / 100;
-                    sleep(100);
-                }
-                // this is for paper arm launcher
-                if (gamepad1.dpad_left) {
-                    error_corection = error_corection - 0.1;
-                }
-                if (gamepad1.dpad_right) {
-                    error_corection = error_corection + 0.1;
-                }
-                // this controlls arm speed
-                if (gamepad2.b) {
-                    if (Arm_speed == 0.25) {
-                        Arm_speed = 0.75;
-                    } else {
-                        Arm_speed = 0.25;
-                    }
-                    sleep(100);
-                }
-                // enabling debug mode
-                // Printing stuff to the screens
-                // True will actvate debug info
+                telemetry.addData("\nbreak", "\n");
+                telemetry.addData("FL_Max_Power", FL_Max_Power);
+                telemetry.addData("FR_Max_Power", FR_Max_Power);
+                telemetry.addData("FL_Max_Power", BL_Max_Power);
+                telemetry.addData("FL_Max_Power", BR_Max_Power);
+                telemetry.addData("\nbreak", "\n");
+                telemetry.addData("FL_Max_Power", FL_Max_Power);
+                telemetry.addData("FR_Max_Power", FR_Max_Power);
+                telemetry.addData("FL_Max_Power", BL_Max_Power);
+                telemetry.addData("FL_Max_Power", BR_Max_Power);
+
+
+
+                // Controlls front  and back movment
+                BL_Power += gamepad1.left_stick_y;
+                BR_Power += gamepad1.left_stick_y;
+                FL_Power += gamepad1.left_stick_y;
+                FR_Power += gamepad1.left_stick_y;
+                // For turning left and right
+                BL_Power += gamepad1.left_trigger + gamepad1.right_trigger * -1;
+                BR_Power += gamepad1.right_trigger + gamepad1.left_trigger * -1;
+                FL_Power += gamepad1.left_trigger + gamepad1.right_trigger * -1;
+                FR_Power += gamepad1.right_trigger + gamepad1.left_trigger * -1;
+                //  For moving left and right is also compatle with forword and backword to allow moving in all directions
+                BL_Power += gamepad1.right_stick_x;
+                BR_Power += gamepad1.right_stick_x * -1;
+                FL_Power += gamepad1.right_stick_x * -1;
+                FR_Power += gamepad1.right_stick_x;
+
+
+
+
+
+
                 hook.setPower(gamepad2.left_stick_y);
-                BL.setPower(BL2 * MAX_SPEED);
-                BR.setPower(BR2 * MAX_SPEED);
-                FL.setPower(FL2 * MAX_SPEED);
-                FR.setPower(FR2 * MAX_SPEED);
+                BL.setPower(BL_Power * BL_Max_Power);
+                BR.setPower(BR_Power * BR_Max_Power);
+                FL.setPower(FL_Power * FL_Max_Power);
+                FR.setPower(FR_Power * FR_Max_Power);
                 // RESETS SO THAT THERE ISNT A INFINTE ADD LOOP
-                BL2 = 0;
-                BR2 = 0;
-                FL2 = 0;
-                FR2 = 0;
+                Partal_reset_mem();
                 telemetry.update();
             }
         }
