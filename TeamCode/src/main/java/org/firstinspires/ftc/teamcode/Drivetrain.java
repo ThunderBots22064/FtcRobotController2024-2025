65de3e279b0aa8ae786130dcd91e8074dfadd8bc
BotEECraze <evsta28@icstudents.org>
2024-11-04 23:26:00 +0000

    Added some basic drive code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
@@ -1,5 +1,43 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.*;
+import java.lang.Math.*;
+
 public class Drivetrain {
+    private DcMotor[4] motors = null;
+
+    public Drivetrain(HardwareMap hardwareMap) {
+        String[4] motorMaps = ["frontLeft", "frontRight",
+                               "backLeft", "backRight"];
+        for (int i = 0; i < motors.length; i++) {
+            motors[i] = hardwareMap.get(DcMotor.class, motorMaps[i]);
+        }
+    }
+
+    /**
+     * drive - A function to drive the robot in a certain direction
+     * angle - A value from 0 to 360 degrees, 0 degrees is a heading straight foward relative to the robot, 90 degrees is straight left
+     * power - A double from -1.0 to 1.0 specifying the power
+     */
+    public void drive(int angle, double power) {
+        // Convert the angle into radians
+        double theta = Math.toRadians(angle);
+        // Rotate the angle 45 degrees over (PI / 4)
+        theta -= (Math.PI / 4.0);
+
+        double forward = Math.cos(theta) * power;
+        double side = Math.sin(theta) * power;
+
+        motors[0].setPower(forward);
+        motors[4].setPower(forward);
+        motors[1].setPower(side);
+        motors[2].setPower(side); 
+    }
+
+    public void stop() {
+        for (int i = 0; i < motors.length; i++) {
+            motors.setPower(0);
+        }
+    }
 
 }
