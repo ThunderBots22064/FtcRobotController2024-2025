d5165ea44201cbd4417e37d802e59dd77196e03d
strangejmaster <strangejmaster@gmail.com>
2024-11-05 16:42:34 -0600

    Added wrist control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Claw.java
@@ -5,6 +5,10 @@ import com.qualcomm.robotcore.hardware.*;
 public class Claw {
     private Servo claw;
     private Servo wrist;
+
+    double wristPos = 0;
+    double wristIncrement = 0.05;
+
     public Claw (HardwareMap hardwareMap) {
         claw = hardwareMap.get(Servo.class, "claw");
         wrist = hardwareMap.get(Servo.class, "wrist");
@@ -19,5 +23,22 @@ public class Claw {
 
     public void close() {
         claw.setPosition(0); //temp position
+    }
+
+    public void up() {
+        if (wristPos + wristIncrement < 1.0) {
+            wristPos += wristIncrement;
+        } else {
+            wristPos = 1.0;
+        }
+        wrist.setPosition(wristPos);
+    }
+    public void down() {
+        if (wristPos - wristIncrement > 0) {
+            wristPos -= wristIncrement;
+        } else {
+            wristPos = 0;
+        }
+        wrist.setPosition(wristPos);
     }
 }
