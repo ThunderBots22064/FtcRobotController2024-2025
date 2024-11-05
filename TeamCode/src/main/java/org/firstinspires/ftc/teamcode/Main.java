66395301ddea2c6de9a7cd8ec32eb2b09af1f833
strangejmaster <strangejmaster@gmail.com>
2024-11-05 14:27:35 -0600

    Added field-oriented driving

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -3,15 +3,24 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.Subsystems.*;
+import org.firstinspires.ftc.teamcode.Utils.*;
+
 @TeleOp
 public class Main extends OpMode {
     ViperSlide slide;
     Drivetrain drivetrain;
+    Imu imu;
+
+    boolean fieldOriented = false;
+    OnPress orientSwitch = new OnPress();
+    OnPress orientReset = new OnPress();
 
     @Override
     public void init() {
         slide = new ViperSlide(hardwareMap);
         drivetrain = new Drivetrain(hardwareMap);
+        imu = new Imu(hardwareMap);
     }
 
     @Override
@@ -24,6 +33,16 @@ public class Main extends OpMode {
             slide.stop();
         }
 
+        // Toggle field oriented modes only when both bumpers are pressed (Without repeatedly toggling)
+        if (orientSwitch.pressed(gamepad1.left_bumper && gamepad1.right_bumper)) {
+            fieldOriented = !fieldOriented;
+        }
+
+        // If 'B' has been pressed and the robot is in field-oriented mode reset the IMU
+        if (orientReset.pressed(gamepad1.b) && fieldOriented) {
+            imu.resetHeading();
+        }
+
 //        Calculate the value from the controller to give to the drivetrain
         double xComponent = deadzone(gamepad1.left_stick_x, 0.15);
         // Note that the y-value on joysticks is negative for forward values so it's reversed here
@@ -31,6 +50,12 @@ public class Main extends OpMode {
         double magnitude = Math.sqrt(Math.pow(xComponent, 2) + Math.pow(yComponent, 2));
         double angle = Math.atan2(yComponent, xComponent);
         double turn = deadzone(gamepad1.right_trigger, 0.1) - deadzone(gamepad1.left_trigger, 0.1);
+
+        // Use the IMU to calculate the desired angle in field-oriented mode
+        if (fieldOriented) {
+            angle -= imu.getRawHeading();
+        }
+
         drivetrain.drive(angle, magnitude, turn);
         //        telemetry.add("Slide target: ", slide.getTarget());
     }
