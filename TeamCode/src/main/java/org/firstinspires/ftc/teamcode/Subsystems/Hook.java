d1bd079d66d1a332d659aa7090b92e206ef25b9c
strangejmaster <strangejmaster@gmail.com>
2024-11-05 14:43:29 -0600

    Created Hook, very similar to ViperSlide

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Hook.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.qualcomm.robotcore.hardware.*;
+
+public class Hook {
+    final private DcMotor hook;
+
+    private int target = 0;
+
+    // Define the floor and ceiling of the hook
+    final int ceil = 200;
+    final int floor = 0;
+
+    public Hook(HardwareMap hardwareMap) {
+        hook = hardwareMap.get(DcMotor.class, "hook");
+        hook.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+    public int getTarget() {
+        return target;
+    }
+
+    public void up() {
+        target += 10;
+        if (target > ceil) {
+            target = ceil;
+        }
+        hook.setTargetPosition(target);
+    }
+
+    public void down() {
+        target -= 10;
+        if (target < floor) {
+            target = floor;
+        }
+        hook.setTargetPosition(target);
+    }
+
+    public void stop() {
+        hook.setPower(0);
+    }
+
+
+}
