8bab45691ef8367e3c353af68b7e99ed288b40b6
strangejmaster <strangejmaster@gmail.com>
2024-11-05 15:37:26 -0600

    Added wrist servo (WIP)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Claw.java
@@ -1,10 +1,13 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 import com.qualcomm.robotcore.hardware.*;
 
+// IMPLEMENT WRIST!!!!
 public class Claw {
     private Servo claw;
+    private Servo wrist;
     public Claw (HardwareMap hardwareMap) {
         claw = hardwareMap.get(Servo.class, "claw");
+        wrist = hardwareMap.get(Servo.class, "wrist");
     }
 
     public void open() {
