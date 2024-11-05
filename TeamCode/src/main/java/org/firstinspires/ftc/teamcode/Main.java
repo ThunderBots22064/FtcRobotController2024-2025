b9bcc2607937180414318b52f383690be73ec765
strangejmaster <strangejmaster@gmail.com>
2024-11-04 20:20:44 -0600

    Added drivetrain to Main

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -5,11 +5,13 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
 public class Main extends OpMode {
-    ViperSlide slide = null;
+    ViperSlide slide;
+    Drivetrain drivetrain;
 
     @Override
     public void init() {
         slide = new ViperSlide(hardwareMap);
+        drivetrain = new Drivetrain(hardwareMap);
     }
 
     @Override
