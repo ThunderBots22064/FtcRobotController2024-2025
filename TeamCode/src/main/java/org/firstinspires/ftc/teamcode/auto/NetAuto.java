4908afb1e479eeabad8395114b92310de22bd4ba
Yashi <yajai25@icstudents.org>
2024-11-08 00:33:37 -0600

    Added roadrunner quickstart files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/NetAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/NetAuto.java
@@ -1,10 +1,10 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.auto;
 
 import com.qualcomm.robotcore.hardware.*;
 import com.qualcomm.robotcore.eventloop.opmode.*;
 
-@Autonomous(name = "RedObservationAuto")
-public class ObservationAuto extends LinearOpMode{
+@Autonomous(name = "RedNetAuto")
+public class NetAuto extends LinearOpMode{
     private DcMotor FR;
     private DcMotor FL;
     private DcMotor BR;
@@ -90,9 +90,10 @@ public class ObservationAuto extends LinearOpMode{
     }
 
     private void slide_Up(double time__in_seconds_, double power){
-        while (slide.getCurrentPosition() < ceil || slide.getCurrentPosition() > floor) {
+        while (opModeIsActive() && (slide.getCurrentPosition() < ceil && slide.getCurrentPosition() > floor)) {
             slide.setPower(power);
             sleep((long) (time__in_seconds_ * 1000));
+
         }
         slide.setPower(0);
         sleep( 2000);
@@ -118,11 +119,39 @@ public class ObservationAuto extends LinearOpMode{
         slide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         slide.setDirection(DcMotorSimple.Direction.FORWARD);
 
+        /*
+        //red net zone side (complex w/ pre-load)
+        forward(0.15,0.25);
+        strafe_Left(1.5,0.25);
+        turn_Left(0.25, 0.25);
+        slide_Up(1,0.25);
+        wrist.setPosition(0);
+        wrist.setPosition(-0.15);
+        claw.setPosition(0.7);
+        backward(0.15, 0.25);
+        turn_Right(0.25, 0.25);
+        forward(1.25,0.25);
+        strafe_Right(1.5,0.25);
+        stop(2);
+         */
 
-        //red observation zone side (simple park)
-        forward(0.1, 0.75); // original time = 0.15s
+        /*
+        //red net zone side (simple park) -- draft 1
+        strafe_Left(0.5, 0.75);
+        forward(1.5,0.75);
+        strafe_Right(0.5, 0.75);
+        slide_Up(0.5,0.75); // too long, maybe half or third
+        stop(2);
+         */
+
+        //edited version
+        strafe_Left(0.5, 0.75);
+        forward(1.3,0.75);
+        stop(1);
+        strafe_Right(0.3, 0.75);
+        stop(1);
         turn_Right(0.4, 0.75);
-        forward(0.5,0.75);
+        slide_Up(0.15,0.75); // too long, maybe half or third
         stop(2);
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/ObservationAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/ObservationAuto.java
@@ -1,10 +1,10 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.auto;
 
 import com.qualcomm.robotcore.hardware.*;
 import com.qualcomm.robotcore.eventloop.opmode.*;
 
-@Autonomous(name = "RedNetAuto")
-public class NetAuto extends LinearOpMode{
+@Autonomous(name = "RedObservationAuto")
+public class ObservationAuto extends LinearOpMode{
     private DcMotor FR;
     private DcMotor FL;
     private DcMotor BR;
@@ -90,10 +90,9 @@ public class NetAuto extends LinearOpMode{
     }
 
     private void slide_Up(double time__in_seconds_, double power){
-        while (opModeIsActive() && (slide.getCurrentPosition() < ceil && slide.getCurrentPosition() > floor)) {
+        while (slide.getCurrentPosition() < ceil || slide.getCurrentPosition() > floor) {
             slide.setPower(power);
             sleep((long) (time__in_seconds_ * 1000));
-
         }
         slide.setPower(0);
         sleep( 2000);
@@ -119,39 +118,11 @@ public class NetAuto extends LinearOpMode{
         slide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         slide.setDirection(DcMotorSimple.Direction.FORWARD);
 
-        /*
-        //red net zone side (complex w/ pre-load)
-        forward(0.15,0.25);
-        strafe_Left(1.5,0.25);
-        turn_Left(0.25, 0.25);
-        slide_Up(1,0.25);
-        wrist.setPosition(0);
-        wrist.setPosition(-0.15);
-        claw.setPosition(0.7);
-        backward(0.15, 0.25);
-        turn_Right(0.25, 0.25);
-        forward(1.25,0.25);
-        strafe_Right(1.5,0.25);
-        stop(2);
-         */
 
-        /*
-        //red net zone side (simple park) -- draft 1
-        strafe_Left(0.5, 0.75);
-        forward(1.5,0.75);
-        strafe_Right(0.5, 0.75);
-        slide_Up(0.5,0.75); // too long, maybe half or third
-        stop(2);
-         */
-
-        //edited version
-        strafe_Left(0.5, 0.75);
-        forward(1.3,0.75);
-        stop(1);
-        strafe_Right(0.3, 0.75);
-        stop(1);
+        //red observation zone side (simple park)
+        forward(0.1, 0.75); // original time = 0.15s
         turn_Right(0.4, 0.75);
-        slide_Up(0.15,0.75); // too long, maybe half or third
+        forward(0.5,0.75);
         stop(2);
     }
 
