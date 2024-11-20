d0854d66b47508bf10541379921a0a42de572f61
Yashi <yajai25@icstudents.org>
2024-11-20 16:20:11 -0600

    Updated Control Hub Orientation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Imu.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Imu.java
@@ -16,8 +16,8 @@ public class Imu {
 
         imu.initialize(
                 new IMU.Parameters(new RevHubOrientationOnRobot(
-                        RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                        RevHubOrientationOnRobot.UsbFacingDirection.FORWARD))
+                        RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
+                        RevHubOrientationOnRobot.UsbFacingDirection.UP))
         );
     }
 
